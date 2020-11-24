509e02de8e075b09faa998acc6d59ff90ab81089
Robert Grimes <rjgrimes@gmail.com>
2020-11-24 15:38:10 -0600

    Initial file upload for React Portfolio

---
+++ src/components/pages/portCards.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function PortCard(props) {
+    // destruct props to contain:
+    // name, image, repo, desc
+    return (
+        <>
+
+        </>
+    )
+}
+
+export default PortCard;
